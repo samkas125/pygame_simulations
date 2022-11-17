e60b9bc44142df40cf12f182a3857d222db41e8c
unknown <samkas125@gmail.com>
2022-11-17 23:00:07 +0530

    bottom line

--- bounce.py
+++ bounce.py
@@ -17,7 +17,7 @@ WINDOW = pygame.display.set_mode((900, 600))
 pygame.display.set_caption('Bouncing ball simulation with coefficient of restitution')
 FPS = 30
 BALL_OBJ = pygame.image.load(os.path.join('assets', 'soccerball.png'))
-font = pygame.font.Font('fonts/font.ttf', 24)
+font = pygame.font.Font(os.path.join('fonts', 'font.ttf'), 24)
 text_color = (255, 255, 255)
 
 
@@ -25,6 +25,8 @@ text_color = (255, 255, 255)
 
 def draw_window(rect, hMax, font, text_color, co_res):
         WINDOW.fill((0, 0, 0))
+        pygame.draw.line(WINDOW, (255, 255, 255), (100, 524), (800, 524))
+        pygame.draw.line(WINDOW, (255, 255, 255), (100, 525), (800, 525))
         WINDOW.blit(BALL_OBJ, (rect.x, rect.y))
         bounceHeight = font.render(f'Height of bounce: {500 - hMax} px', True, text_color)
         coRest = font.render(f'Coefficient of restitution: {co_res * -1}', True, text_color)
@@ -42,7 +44,7 @@ def main(co_res):
     ball_rect = pygame.Rect(450, 100, 24, 24)
    
     draw_window(ball_rect, hMax, font, text_color, co_res)
-    y_change = 1
+    y_change = 0
     while run:
         clock.tick(FPS)
         draw_window(ball_rect, hMax, font, text_color, co_res)
@@ -50,14 +52,19 @@ def main(co_res):
         if keys_pressed[K_SPACE]:
             started = True
         
+        
         if started:
             y_change += 0.3
             ball_rect.y += y_change
             draw_window(ball_rect, hMax, font, text_color, co_res)
         
-        if y_change < 0.15 and y_change >= -0.15:
+        if y_change < 0.15 and y_change >= -0.15 and started:
             print(f'Max height of bounce: {500 - ball_rect.y} px')
             hMax = ball_rect.y
+            if (500-hMax) <= 10:
+                started = False
+                ball_rect.y = 500
+                draw_window(ball_rect, hMax, font, text_color, co_res)
 
         
         
